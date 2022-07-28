bce24f7dfbeb0afd1f3475c7af47fb43ec7c1929
ViniciusChagas <viniciushrcs@gmail.com>
2022-07-28 10:40:21 -0300

    feat: create pipeline of streams to read the file and do some operations

---
+++ index.js
@@ -0,0 +1,30 @@
+const { parse } = require("csv-parse");
+const fs = require("fs");
+
+const habitablePlanets = [];
+
+function isHabitable(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+
+fs.createReadStream("kepler_data.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
+  .on("data", (planet) => {
+    if (isHabitable(planet)) {
+      habitablePlanets.push(planet);
+    }
+  })
+  .on("error", (err) => console.log(err))
+  .on("end", () => {
+    console.log(`${habitablePlanets?.length} habitable planets found`);
+  });
